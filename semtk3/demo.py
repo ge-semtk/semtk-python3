72a72448609f43f691ab53218e3167ca114e5e29
Langston Barrett <langston.barrett@gmail.com>
2020-07-28 10:58:24 -0700

    demo: fix trailing whitespace, typo

--- semtk3/demo.py
+++ semtk3/demo.py
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,52 +27,52 @@ import logging
 
 if __name__ == '__main__':
     semtk3.set_host("http://localhost")
-    
+
     #
     # configure logging:   http://docs.python.org/3/howto/logging.html
     # using:               https://docs.python.org/3/howto/logging-cookbook.html
     #
-    
+
     ### LOGGING ####
     # set up logging globally
     logging.basicConfig(filename=None, level=logging.ERROR);
-    
+
     # set semtk3 logging to DEBUG   (below we switch it to the more typical logging.INFO)
     semtk3.get_logger().setLevel(level=logging.INFO)
-    
+
     if not semtk3.check_services():
         print("A service failed.  Check logs")
-    
+
     # don't want to demo mucking with nodegroup store.  Also need valid local folder path.
     #semtk3.retrieve_from_store("regex", "folder path")
     #semtk3.delete_nodegroup_from_store("nodegroup_id")
     #semtk3.store_nodegroups("folder path")
-    
-    # 
+
+    #
     # get a list of nodegroups
     #
     table = semtk3.get_nodegroup_store_data()
     print(table.get_rows())
-    
-    # 
+
+    #
     # get a table of constraints for a nodegroup_id
     #
     nodegroup_id="Lighting Weekly Units Sold"
     table = semtk3.get_constraints_by_id(nodegroup_id)
     print(table.get_rows())
-    
+
     #
     # logging
     # show only high-level logging (e.g. percent complete)
     #
     semtk3.get_logger().setLevel(level=logging.INFO)
-    
+
     #
-    # get all existings values for the constraint "?pc"
+    # get all existing values for the constraint "?pc"
     #
     table = semtk3.get_filter_values_by_id(nodegroup_id, "?pc")
     print(table.get_rows())
-    
+
     #
     # build a constraint of the first two legal values and run it
     #
@@ -81,13 +81,13 @@ if __name__ == '__main__':
     constraint = semtk3.build_constraint("?pc", semtk3.OP_MATCHES, list2)
     table = semtk3.select_by_id(nodegroup_id, runtime_constraints=[constraint])
     print(table.get_rows())
-    
+
     #
     # run the whole query
     #
     table = semtk3.select_by_id(nodegroup_id)
     print(table.get_rows())
-    
+
     #
     # ingest async with override
     #
@@ -106,21 +106,21 @@ if __name__ == '__main__':
             "graph":"http://paultest/data"
         }]
     }'''
-    
+
     # this type of import cleans up code further:
     from semtk3.sparqlconnection import SparqlConnection
-    
+
     semtk3.clear_graph(TEST_CONN_STR, SparqlConnection.MODEL, 0)
-    
+
     # upload owl
     #OWL_PATH = r'C:\Users\200001934\workspace-kepler\Lighting\OwlModels\Lighting.owl'
     #semtk3.upload_owl(OWL_PATH, TEST_CONN_STR, "dba", "dba")
-    
+
     # sample get oinfo table
     table = semtk3.get_oinfo_uri_label_table(TEST_CONN_STR)
     print(table.get_csv_string())
-    
-    
+
+
     #
     # Execute query against SemTK HiveService
     #
@@ -131,6 +131,6 @@ if __name__ == '__main__':
     #
     # hive_query=""
     # table=semtk3.query_hive("hive-server-host.crd.ge.com", "10000", "databasename", hive_query)
-    #   
+    #
     # convert to numpy array
     # npa = np.array(table.get_rows())
